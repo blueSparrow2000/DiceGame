e429c0e4c45d4de926a0e68d72ba6e449494a225
blueSparrow2000 <amadeus@kaist.ac.kr>
2024-08-09 18:11:32 +0900

    Balancing

--- skill_book.py
+++ skill_book.py
@@ -823,7 +823,6 @@ class Baron_skills(Skill_Book):
             sound_effects['block'].play()
             time.sleep(0.12)
 
-
         D = player.count_tile('Defence')
         player.get_defence(10*D)
         
@@ -834,8 +833,6 @@ class Baron_skills(Skill_Book):
 
         # 지금부터 얻는 디펜스는 전부 템포랄 디펜스로 전환하도록 해야함 (보드 리셋시 다시 초기화되는 플래그)
         player.all_def_to_temporal_def = True
-
-
         # 보드가 리셋될때 템포랄 디펜스도 리셋되고 저 플래그도 리셋됨
 
     def get_detail_Build(self, player):
@@ -892,9 +889,9 @@ class Ato_skills(Skill_Book):
 
 character_skill_dictionary = {'Mirinae':Mirinae_skills(),'Cinavro':Cinavro_skills(), 'Narin': Narin_skills(), 'Baron': Baron_skills(), 'Riri': Riri_skills(), 'Arisu': Arisu_skills(), 'Ato': Ato_skills()}
 character_tile_dictionary = {'Mirinae':{'Attack':8, 'Regen':0, 'Defence':4, 'Skill':4, 'Joker':0, 'Karma':0},
-                             'Cinavro':{'Attack':4, 'Regen':0, 'Defence':6,  'Skill':6, 'Joker':1,'Karma':0},
-                             'Baron':{'Attack':4, 'Regen':0, 'Defence':8,  'Skill':4, 'Joker':0,'Karma':0},
-                             'Narin':  {'Attack':4, 'Regen':0, 'Defence':4,  'Skill':8, 'Joker':0,'Karma':1},
+                             'Cinavro':{'Attack':4, 'Regen':0, 'Defence':4,  'Skill':8, 'Joker':1,'Karma':0},
+                             'Baron':{'Attack':2, 'Regen':0, 'Defence':8,  'Skill':6, 'Joker':0,'Karma':0},
+                             'Narin':  {'Attack':3, 'Regen':0, 'Defence':3,  'Skill':10, 'Joker':0,'Karma':1},
                              'Riri': {'Attack': 2, 'Regen': 6, 'Defence': 2, 'Skill': 6, 'Joker': 0, 'Karma': 0},
                              'Arisu': {'Attack': 3, 'Regen': 1, 'Defence': 5, 'Skill': 7, 'Joker': 0, 'Karma': 0}, # 타일 수 하나 적은 대신 유물 가지고 시작
                              'Ato': {'Attack': 5, 'Regen': 2, 'Defence': 5, 'Skill': 4, 'Joker': 0, 'Karma': 0}, # 타일 수 하나 적은 대신 돈 많이 가지고 시작 (50원)
